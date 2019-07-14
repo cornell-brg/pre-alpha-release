b0cc70fdb774c8fd28dbd4ab8814e8eda947a284
Katie Lim <katielim@cs.washington.edu>
2019-07-13 23:39:58 -0700

    upgrade some infrastructure files

--- bp_common/test/src/demos/src/bp_utils.h
+++ bp_common/test/src/demos/src/bp_utils.h
@@ -2,6 +2,13 @@
 #define BP_UTILS_H
 #include <stdint.h>
 
-void barrier_end(volatile uint64_t *barrier_address, uint64_t total_num_cores);
+typedef struct bp_lock {
+    uint64_t locked;
+} bp_lock_t;
+
+void barrier_end(volatile uint64_t * barrier_num, uint64_t total_num_cores);
+void lock_init(volatile bp_lock_t * lock);
+void lock_spinlock(volatile bp_lock_t * lock);
+void unlock_spinlock(volatile bp_lock_t * lock);
 
 #endif
